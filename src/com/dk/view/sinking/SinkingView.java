41da3d1cd5b4f05ec443b232e962609e32a66070
dkmeteor <93440331@qq.com>
2014-07-30 23:52:15 +0800

    update mask

--- src/com/dk/view/sinking/SinkingView.java
+++ src/com/dk/view/sinking/SinkingView.java
@@ -9,34 +9,44 @@ import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 public class SinkingView extends FrameLayout {
-    private float mPercent;
-    private Paint mPaint;
-    private Bitmap mBitmap;
-    private float mLeft, mTop;
+	private float mPercent;
+	private Paint mPaint;
+	private Bitmap mBitmap;
+	private float mLeft, mTop;
 
-    public SinkingView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        mBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.mask);
-    }
+	public SinkingView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mBitmap = BitmapFactory.decodeResource(context.getResources(),
+				R.drawable.mask);
+		mBitmap = mBitmap.createScaledBitmap(mBitmap, mBitmap.getWidth(), 100,
+				false);
+	}
 
-    public void setPercent(float percent) {
-        mPercent = percent;
-        postInvalidate();
-    }
+	public void setPercent(float percent) {
+		mPercent = percent;
+		postInvalidate();
+	}
 
-    public void clear() {
+	public void clear() {
 
-    }
-    
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        super.dispatchDraw(canvas);
-        canvas.drawBitmap(mBitmap, mLeft, -mBitmap.getHeight() * mPercent, mPaint);
-    }
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-    }
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		super.dispatchDraw(canvas);
+		canvas.drawBitmap(mBitmap, -mLeft, -mBitmap.getHeight() * mPercent,
+				mPaint);
+		mLeft += 10;
+
+		if (mLeft >= 500) {
+			mLeft = 0;
+		}
+
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
 
 }
